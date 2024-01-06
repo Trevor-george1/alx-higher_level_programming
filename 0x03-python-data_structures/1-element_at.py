0a9baeed7c186738c7a6b997cb07efc4632062af
Trevor-george1 <Trevormahinda@gmail.com>
2024-01-06 05:05:35 -0800

    update: conditional statements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@ def element_at(my_list, idx):
     count = len(my_list)
     if idx < 0:
         return None
-    elif idx > count:
+    if idx > count:
         return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
