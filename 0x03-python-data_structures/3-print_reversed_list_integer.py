536bca92dcbd423487a51a0e03bf83eace57babd
Trevor-george1 <Trevormahinda@gmail.com>
2024-01-06 05:25:56 -0800

    create: reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-    new = my_list[::-1]
-    for i in new:
-        print("{:d}".format(i))
+    else:
+        new = my_list[::-1]
+        for i in new:
+            print("{:d}".format(i))
