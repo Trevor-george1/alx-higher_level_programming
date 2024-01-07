431e632beafae6135a52a2692be728056bde2b7f
Trevor-george1 <Trevormahinda@gmail.com>
2024-01-07 03:28:16 -0800

    update: school was the worst

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=' ')
+            print("{:d}".format(i), end=' ' if i != row[-1] else "")
         print()
