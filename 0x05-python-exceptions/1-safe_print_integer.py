2a34b536e66a5895b2ed89a06d1442fab55602bd
Trevor <Trevormahinda@gmai.com>
2024-01-22 12:46:00 +0300

    create: print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{}".format(value))
-            return True
-        else:
-            return False
-    except Exception as e:
-        print("{}".format(e))
+        print("{}".format(value))
+        return True
+    except ValueError:
+        return False
