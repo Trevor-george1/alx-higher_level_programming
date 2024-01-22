ac78636cc84e99b3e6f62b6258c50714ec825cc8
Trevor <Trevormahinda@gmai.com>
2024-01-22 12:32:05 +0300

    create: print x element of list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list:
+            if i <= x:
+                print("{}".format(i), end="")
+                count += 1
+        print()
+    except Exception as e:
+        print("{}".format(e))
+    finally:
+        return count
