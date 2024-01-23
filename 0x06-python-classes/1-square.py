6d9e866a1c0854c708a6ab04f84c39466db49bff
Trevor <Trevormahinda@gmai.com>
2024-01-23 12:36:36 +0300

    create: private attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Squuare that defines a square based on 0-square.py"""
+
+
+class Square:
+    """initialization of square with size private"""
+    def __init__(self, size):
+        self.__size = size
