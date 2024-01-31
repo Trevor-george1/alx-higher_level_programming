95ca4bf370cd4de001bff0ee75ec2527b1c791c5
Trevor <Trevormahinda@gmai.com>
2024-01-31 13:39:08 +0300

    Updats

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+'''
+    function that adds two numbers
+    raise exception if numbers are not integers or float
+'''
+
+
 def add_integer(a, b=98):
+    """
+        add numbers and returns sum
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
