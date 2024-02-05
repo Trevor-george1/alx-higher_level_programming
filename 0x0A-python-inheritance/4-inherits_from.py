29c707fd14819a1547b7502bd166e154b08d755f
Trevor <Trevormahinda@gmai.com>
2024-02-05 17:40:40 +0300

    create: isinstance type

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that checksif an objct is an
+instance of a class directly or indirect"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True or False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
