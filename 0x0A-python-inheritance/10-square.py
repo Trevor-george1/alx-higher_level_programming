1e060bbebf1ae95af4085c8599a8bca9f2a39e01
Trevor <Trevormahinda@gmai.com>
2024-02-05 21:24:26 +0300

    create: super

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """constructor"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    """area"""
+    def area(self):
+        return self.__size * self.__size
+    """__str__"""
