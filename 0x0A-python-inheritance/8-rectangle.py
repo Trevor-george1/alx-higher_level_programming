310ebb1f68e6fca4c3ae36dac2df8d5d9e93ae14
Trevor <Trevormahinda@gmai.com>
2024-02-05 20:55:16 +0300

    create: inherit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """class rectangle"""
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """constructor"""
     def __init__(self, width, height):
