37a19f02be583089ea65f543c7fe0348a6f6fa01
Trevor <Trevormahinda@gmai.com>
2024-03-12 12:53:54 +0300

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,7 @@
 -- converts hbtn_0c_0 databse to UTF8
 -- convert Database, first_table, name in first_table
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(255)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
