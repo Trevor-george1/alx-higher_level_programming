71c2ecd8f90de7de740b9b4a04f43239e210b4ae
Trevor <Trevormahinda@gmai.com>
2024-03-13 17:05:05 +0300

    create: cities by state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@
 -- use only one SELECT
 SELECT cities.id, cities.name, states.name as name
 FROM cities
-INNER JOIN states on cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
