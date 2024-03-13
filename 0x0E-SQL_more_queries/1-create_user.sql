97405ceb71631d843c352b4291b4dc3475a9e0c7
Trevor <Trevormahinda@gmai.com>
2024-03-13 13:04:26 +0300

    update: permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,3 @@
 -- should not fail if user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1-pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
