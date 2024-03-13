9d6f8e2228ab5d9879132ed72c4bd7d2af86cfa6
Trevor <Trevormahinda@gmai.com>
2024-03-13 12:57:22 +0300

    create: create user and grant permission

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a user and grants all privileges
+-- password should be user_0d_1_pwd
+-- should not fail if user already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1-pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
