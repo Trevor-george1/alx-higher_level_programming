6426eeaa5c26e009ca4d1f88d24a4be402d5b574
Trevor <Trevormahinda@gmai.com>
2024-03-13 13:03:57 +0300

    update: permissions

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates a user and grants all privileges
 -- password should be user_0d_1_pwd
 -- should not fail if user already exists
-CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1-pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1-pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
