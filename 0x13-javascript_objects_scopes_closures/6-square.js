4ae1d0637a61764dae6f4d75e0aa1a52d1a3b4ef
Trevor <Trevormahinda@gmai.com>
2024-04-09 15:21:54 +0300

    bullets

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
