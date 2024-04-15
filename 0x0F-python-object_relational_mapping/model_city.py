a9fd373610e24950ef6799a50d7b12668ce876f7
Trevor <Trevormahinda@gmail.com>
2024-04-15 13:47:22 +0300

    streer

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class definition off class city"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Module of a city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
